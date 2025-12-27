71d785db6500654d003ced9b6aa0a96aee60aeaf
andrias261 <128659293+andrias261@users.noreply.github.com>
2025-12-27 15:08:36 +0700

    Penambahan struktur rtos untuk master

--- Kode_Program/master_program.ino
+++ Kode_Program/master_program.ino
@@ -1,8 +1,51 @@
-void setup(){
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+void TaskBacaSensor(void *pvParameters);
+void TaskSendCloud(void *pvParameters);
+
+void setup() {
+  Serial.begin(9600);
+
+  xTaskCreatePinnedToCore(
+    TaskBacaSensor, "baca_sensor_dari_slave"
+    , 
+    8000 //stack size
+    ,
+    NULL, 2 //Priority
+    ,
+    NULL, ARDUINO_RUNNING_CORE);
+
+  xTaskCreatePinnedToCore(
+    TaskSendCloud, "kirim_data_mqtt"
+    , 
+    8000 //stack size
+    ,
+    NULL, 2 //Priority
+    ,
+    NULL, ARDUINO_RUNNING_CORE);
+  
+}
+
+void loop() {
 
 }
 
+void TaskBacaSensor(void *pvParameters) {
+  (void)pvParameters;
+  for (;;) {
+    //Task
+    vTaskDelay(100);
+  }
+}
 
-void loop(){
-    
+void TaskSendCloud(void *pvParameters) {
+  (void)pvParameters;
+  for (;;) {
+    //Task
+    vTaskDelay(100);
+  }
 }
